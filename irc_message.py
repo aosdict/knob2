d880a497b0d38b026343d83fbf786bd11830e431
Cameron Root <croot@redhat.com>
2016-07-06 17:11:32 -0400

    Fixed parsing no-colon prefixes, added exceptions to parsing, added string conversion and name extraction

--- irc_message.py
+++ irc_message.py
@@ -3,6 +3,11 @@
    with.
 """
 
+import sys
+
+# stop creating .pyc files
+sys.dont_write_bytecode = True
+
 class IrcMessage:
    prefix = ""
    command = ""
@@ -11,18 +16,24 @@ class IrcMessage:
 
    # Constructor takes a line verbatim from an IRC server, which is then parsed
    # into the correct components. 
-   def __init__(self, irc_line):
-      # IRC lines are broken into [:]<prefix> <command> [ <param> ... ] :<trail>
-    
+   def __init__(self, line):
+      # IRC lines are broken into [:<prefix>] <command> [ <param> ... ] :<trail>
+
       # get prefix
-      prefixStart = 1 if (line[0] == ":") else 0
-      prefixEnd = line.find(" ")
+      if line[0] == ':':
+         prefixStart = 1
+         prefixEnd = line.find(" ")
+         if prefixEnd == -1:
+            raise ValueError('Could not find end of prefix in line ' + line)
+      else:
+         prefixStart = 0
+         prefixEnd = 0
       self.prefix = line[prefixStart:prefixEnd]
 
       # get trail, which is unique in that it is the first part of the line
       # to begin with " :"
       trailStart = line.find(" :")
-      if trailStart > 0:
+      if trailStart >= 0:
          commandEnd = trailStart
          trailStart += 2
       else:
@@ -31,12 +42,22 @@ class IrcMessage:
       self.trail = line[trailStart:]
 
       # command and params are between the prefix and trail
-      commandStart = prefixEnd+1
-      commandString = line[commandStart:commandEnd].split();
-      self.command = commandString[0]
-      self.params = commandString[1:]
-      
+      commandStart = prefixEnd+1 if prefixEnd > 0 else 0
+      commandList = line[commandStart:commandEnd].split();
+      if len(commandList) < 1:
+         raise ValueError('Could not find a command in line ' + line)
+      self.command = commandList[0]
+      self.params = commandList[1:]
 
-    # If the prefix is user!~server.com or similar, gets "user" from it
-    def getName(self):
-        return extractName(self.prefix)
+
+   # String conversion
+   def __str__(self):
+      string_form = 'PREFIX: "%s" COMMAND: "%s" PARAMS: "%s" TRAIL: "%s"'
+      return string_form % (self.prefix, self.command, " ".join(self.params), self.trail)
+
+
+   # If the prefix is user!~server.com or similar, gets "user" from it
+   def getName(self):
+      return self.prefix.split('!')[0]
+
+
