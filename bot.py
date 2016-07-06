b6015f32b46186e7551f27f9cc689c0f19b49313
Cameron Root <croot@redhat.com>
2016-07-06 17:09:58 -0400

    Added hooks, interacts, connect, join to bot module

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ import socket
 import select
 import errno
 
+import irc_message
+
 # stop creating .pyc files
 sys.dont_write_bytecode = True
 
@@ -17,35 +19,48 @@ class Bot:
    settings = {}
    hooks = {}
 
-   def __init__(self, settings = {}):  
+   def __init__(self, settings = {}):
       self.sock = None
       self.settings = settings
       # hooks is a dictionary mapping IRC command strings, 
       # like 'PRIVMSG', 'NICK' or '224', to functions that will be called with
-      # a IRC object whenever a message of that command is received.
-      self.hooks = {}
+      # this bot instance and an IRC object whenever the bot receives a message 
+      # of that command.
+      # By default it automatically responds to PING messages with pongs.
+      self.hooks = {
+         'PING': self._pong
+      }
+      # TODO: add hooks for successful channel join,
 
 
    # Send a string to the IRC server over the socket. This just removes the
    # boilerplate \r\n on everything.
-   def __socksend(line):
+   def __socksend(self, line):
       self.sock.send(line + '\r\n')
 
-   def __get_line():
-      data = self.sock.recv(1024)
+   # Get a line from the IRC server. Raises an EOFError if the connection is closed for some reason,
+   # and also takes care of the \r\n on the end of every line.
+   def __get_line(self):
+      data = ''
+      c = ''
+      while c != '\n':
+         c = self.sock.recv(1)
+         data += c
       if not data:
          raise EOFError('Connection closed unexpectedly')
-      
+
+      return data.rstrip('\r\n')
 
 
    # Open connection to an IRC server.
+   # This has a while loop parsing commands, but it will exit 
+   # once it is alerted to the connection message.
    # This isn't expected to be able to be on multiple servers at once.
    # May be called before or after setting hooks.
-   def connect(self, server, nick, port=6667, ident="x", realname="x", channels=[]):
-      self.nick = nick
-      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+   def connect(self, server, nick, port=6667, ident="x", realname="x"):
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       try:
-         s.connect((server, port))
+         self.sock.connect((server, port))
       except socket.error as serr:
          if serr.errno == errno.ECONNREFUSED:
             print 'Connection error: Connection refused by server'
@@ -55,6 +70,94 @@ class Bot:
             print variables.server,'connection error:',errno.errcode[serr.errno]
          raise serr
 
+      attempt_nick = nick
       self.__socksend('USER %s %s %s :%s' % (ident, server, server, realname))
-      self.__socksend('NICK %s' % nick)
+      self.__socksend('NICK %s' % attempt_nick)
+
+      command = ""
+      # wait for 001 (successful connection to the server)
+      while command != "001":
+         try:
+            msg = irc_message.IrcMessage(self.__get_line())
+         except EOFError as e:
+            print 'Connection closed while waiting for 001'
+            raise e
+         except ValueError as e:
+            print 'Problem parsing received line: %s' % e
+
+         command = msg.command
+         if command == '001':
+            print 'Connection succeeded'
+            self.nick = attempt_nick
+         elif command == '433':
+            print 'Nickname', attempt_nick, 'was already in use'
+            attempt_nick += '_'
+            print 'Trying with', attempt_nick
+            self.__socksend('NICK ' + attempt_nick)
+         else:
+            print 'Unknown IRC command:', msg
+
+
+   # Attempts to join a channel.
+   # TODO: allow this to take a list of channels
+   def join(self, channelName):
+      if len(channelName) < 1:
+         print 'Warning: tried to call join with an empty channel name, ignoring'
+         return
+      if channelName[0] != '#':
+         self.__socksend('JOIN #'+channelName)
+      else:
+         self.__socksend('JOIN '+channelName)
+
+
+   # Add a new hook. This assigns a function to a certain type of command.
+   # Preferably, hooks should be added prior to connecting.
+   def add_hook(self, command, fn):
+      self.hooks[command] = fn
+
+
+   # Make the bot say a message to a given channel or nick.
+   def say(self, msg_str, recipient):
+      self.__socksend('PRIVMSG %s :%s' % (recipient, msg_str))
+
+
+   # Interacts with the IRC server. This will start a loop that will not exit until the bot is
+   # terminated manually or the server closes the connection. Inside the loop, the bot will
+   # read and interpret commands according to its hooks.
+   def interact(self):
+      while True:
+         read_socks, write_socks, err_socks = select.select([sys.stdin, self.sock], [], [])
+
+         for sock in read_socks:
+            if sock == sys.stdin:
+               user_input = sys.stdin.readline().rstrip('\n')
+               if user_input.lower() == 'quit':
+                  return
+
+            if sock == self.sock:
+               try:
+                  msg = irc_message.IrcMessage(self.__get_line())
+                  print msg
+               except EOFError:
+                  print 'Connection closed unexpectedly'
+                  return
+               except ValueError as e:
+                  print 'Problem parsing received line: %s' % e
+
+               if msg.command in self.hooks:
+                  self.hooks[msg.command](self, msg)
+               else:
+                  pass
+                  # print 'No hook found'
+
+
+   ### 
+   ### Default hook functions
+   ### These should all have 1 preceding underscore
+   ### 
+
+   # Send a PONG to the server.
+   def _pong(self, bot, msg):
+      self.sock.send('PONG :Pong')
+
 
