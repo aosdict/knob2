585a218998eb88efc9684e3a44bcc5b7905a160d
aosdict <aosdict@gmail.com>
2016-07-07 07:34:01 -0400

    Added more things to main

--- main.py
+++ main.py
@@ -4,21 +4,38 @@
 
 import bot
 import irc_message
+import pymongo
+import sys
+import re
 
-settings = {
-}
+mclient = pymongo.MongoClient('localhost', 27017)
+db = mclient['jbot']
 
 def privmsg_fn(bot, msg):
    sender = msg.getName()
    recipient = msg.params[0]
+   message = msg.trail
+
    if recipient == bot.nick:
-      bot.say(msg.trail, sender)
+      # private message
+      recipient = sender
    else:
-      bot.say(msg.trail, recipient)
+      # public message in a channel
+
+      # look for a ++ or -- in the string (karma up/down)
+      karma_mod_list = re.findall('[^ ]+[+-][+-]', message)
+
+         
+      # add this message to the database of quotes for this sender
+
+   bot.say(msg.trail, recipient)
+
+settings = {
+}
 
 
 jbot = bot.Bot(settings)
 jbot.add_hook('PRIVMSG', privmsg_fn)
-jbot.connect('irc.devel.redhat.com', 'hfuss')
-jbot.join('#z')
+jbot.connect('irc.freenode.net', 'sjdhfkj')
+jbot.join('#zszszs')
 jbot.interact()
