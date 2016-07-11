b49f646387a57b51ee42e6c27a8dfcbf8d84de46
Cameron Root <croot@redhat.com>
2016-07-11 16:33:59 -0400

    Added Extension class as a parent class for extensions

---
+++ extension.py
@@ -0,0 +1,39 @@
+"""
+   Extension module. Abstract base class for any extensions that
+   a programmer wants to register.
+"""
+
+# import abc
+import sys
+
+import irc_message
+
+# stop creating .pyc files
+sys.dont_write_bytecode = True
+
+class Extension(object):
+   # # define metaclass properly as abstract
+   # __metaclass__ = abc.ABCMeta
+
+   # variables
+   name = "" # expected to be supplied by the implementor
+   hooks = {}
+   bot = None
+
+
+   def __init__(self, bot):
+      self.bot = bot
+
+
+   def add_hook(self, command, fn):
+      self.hooks[command] = fn
+
+
+   # Act on an irc message. This should not be called by the programmer.
+   # Return a truth value representing whether the bot should continue
+   # trying to call act on other messages.
+   def act(self, msg):
+      if msg.command not in self.hooks:
+         return True
+
+      return self.hooks[msg.command](msg)
