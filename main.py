dd46a2de186fbfe375990ed627bb668ead31e375
Cameron Root <croot@redhat.com>
2016-07-20 09:09:53 -0400

    Removed old command handling hook code

--- main.py
+++ main.py
@@ -18,41 +18,6 @@ import extensions.sundry_commands as sundry_commands
 mclient = pymongo.MongoClient('localhost', 27017)
 db = mclient['jbot']
 
-'''
-# Handle a command to the bot. Commands could be virtually anything.
-# Returns True if the message should not be parsed after handling the command.
-def handle_command(bot, message, sender, original_recipient):
-   cmd_list = message.split()
-   # cmd_list[0] is expected to be the bot's name
-   if len(cmd_list) < 2:
-      # can't really do anything without a command
-      return False
-
-   if cmd_list[1] == 'quote':
-      # pick random number mod numquotes and get the quote with that index
-      numquotes = db.quotes.count()
-      quotenum = random.randint(0, numquotes-1)
-      quote = db.quotes.find_one({'index': quotenum})
-      recipient = sender if original_recipient == bot.nick else original_recipient
-      bot.say('"' + quote['quote'] + '" -- ' + quote['author'], recipient)
-      return True
-
-   return False
-
-
-# Hook for PRIVMSG commands and reacting to them. This will be the biggest part of most bots.
-def privmsg_fn(bot, msg):
-   sender = msg.getName()
-   recipient = msg.params[0]
-   message = msg.trail
-
-   # look for a command to the bot (denoted by any string starting with the bot's nick)
-   if message.find(bot.nick) == 0:
-      if handle_command(bot, message, sender, recipient):
-         return
-
-'''
-
 # handle command-line args
 if len(sys.argv) != 4:
    print 'Usage:', sys.argv[0], 'server nick channel_list'
